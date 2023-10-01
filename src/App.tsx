fb7e39ff75c9e093fc0bb95a267128e50ded7fa1
Nazifullah <nazif.it@gmail.com>
2023-10-02 02:05:48 +0530

    installed chakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
   return (
     <>
       <h2>Welcom to MY gameInfo App </h2>
+      <Button colorScheme="blue">Button</Button>
     </>
   );
 }
