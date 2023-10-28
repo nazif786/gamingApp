1627ec36625d68f4eb2e12a0a7472ca6d3658c37
Nazifullah <nazif.it@gmail.com>
2023-10-29 02:30:48 +0530

    displaying platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,6 +1,35 @@
 import { Platforms } from "../services/games-service";
-import { Text } from "@chakra-ui/react";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaApple,
+  FaAndroid,
+  FaPlaystation,
+  FaXbox,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { IconType } from "react-icons";
 
+// index signature: [key, string] it represents a key or property of this object
+// like here we have pc, mac etc...by using thge index signature we don't have to specify
+// the name of these keys. if {key, string} defines it's type, when wraped in [] we are defining
+// an index signature, so it simply says this object has any numaber of keys of type string.
+// and each key is mapped to the value of IconType which is defined in react-icon library.
+//  with that the error is gone.
+const iconMap: { [key: string]: IconType } = {
+  pc: FaWindows,
+  android: FaAndroid,
+  xbox: FaXbox,
+  linux: FaLinux,
+  playstation: FaPlaystation,
+  mac: FaApple,
+  ios: MdPhoneIphone,
+  nintendo: SiNintendo,
+  web: BsGlobe,
+};
 interface Props {
   platforms: Platforms[];
 }
@@ -8,9 +37,11 @@ interface Props {
 const PlatformIconList = ({ platforms }: Props) => {
   return (
     <>
-      {platforms.map((platform) => (
-        <Text> {platform.name} </Text>
-      ))}
+      <HStack marginY={1}>
+        {platforms.map((platform) => (
+          <Icon as={iconMap[platform.slug]} color="gray.500" />
+        ))}
+      </HStack>
     </>
   );
 };
--- src/services/games-service.ts
+++ src/services/games-service.ts
@@ -1,5 +1,4 @@
 import create from "./http-service";
-
 export interface Platforms {
   id: number;
   name: string;
