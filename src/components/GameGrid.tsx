b159135d8ca45c5fa7eea9547fab4b21755420cb
Nazifullah <nazif.it@gmail.com>
2023-10-29 04:10:38 +0530

    displaying skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,16 @@
 import useGams from "../hooks/useGams";
-import { Text, Spinner, SimpleGrid } from "@chakra-ui/react";
+import { Text, Spinner, SimpleGrid, Skeleton } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 const GameGrid = () => {
   const { games, error, laoding } = useGams();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8];
   return (
     <>
       <h1>Game Grid</h1>
       <br />
-      {laoding && (
+      {/* {laoding && (
         <Spinner
           thickness="4px"
           speed="0.65s"
@@ -16,13 +18,15 @@ const GameGrid = () => {
           color="blue.500"
           size="xl"
         />
-      )}
+      )} */}
       {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         spacing={10}
         padding="10px"
       >
+        {laoding &&
+          skeletons.map((skeleton) => <GameCardSkeleton key={skeleton} />)}
         {games.map((game) => (
           // <li key={game.id}>{game.name}</li>
           <GameCard key={game.id} game={game} />
