aa967cf26b56de60f87ee3042eb16862de87e0eb
Nazifullah <nazif.it@gmail.com>
2023-10-29 02:57:32 +0530

    optimizing images using image-url.ts

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,15 +1,8 @@
-import {
-  Box,
-  Card,
-  CardBody,
-  HStack,
-  Heading,
-  Image,
-  Text,
-} from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../services/games-service";
 import PlatformIconList from "./PlatformIconList";
 import { CriticScore } from "./CriticScore";
+import getCroppedImageUrl from "../services/image-url";
 interface Props {
   game: Game;
 }
@@ -18,7 +11,7 @@ const GameCard = ({ game }: Props) => {
   return (
     <>
       <Card borderRadius={10} overflow="hidden">
-        <Image src={game.background_image} />
+        <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
           <Heading fontSize="2xl">{game.name}</Heading>
           {/* {game.parent_platforms.map(({ platform }) => (
--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -39,7 +39,11 @@ const PlatformIconList = ({ platforms }: Props) => {
     <>
       <HStack marginY={1}>
         {platforms.map((platform) => (
-          <Icon as={iconMap[platform.slug]} color="gray.500" />
+          <Icon
+            as={iconMap[platform.slug]}
+            key={platform.id}
+            color="gray.500"
+          />
         ))}
       </HStack>
     </>
