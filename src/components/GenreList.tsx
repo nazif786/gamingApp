9d149a4f1b5809b8e0bcae14f4f16f640df01bf2
Nazifullah <nazif.it@gmail.com>
2023-10-30 10:01:55 +0530

    displaying genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,26 @@
+import { Text, HStack, Image, List, ListItem } from "@chakra-ui/react";
 import useGenres from "../hooks/useGenres";
+import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   const { data } = useGenres();
   // console.log(data);
   return (
     <>
-      <ul>
+      <List>
         {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+          <ListItem key={genre.id} paddingY="5px">
+            <HStack>
+              <Image
+                src={getCroppedImageUrl(genre.image_background)}
+                boxSize="32px"
+                borderRadius={8}
+              />
+              <Text fontSize="lg">{genre.name}</Text>
+            </HStack>
+          </ListItem>
         ))}
-      </ul>
+      </List>
     </>
   );
 };
