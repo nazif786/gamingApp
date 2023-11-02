fe908d2c2c3ba81beab0bbd2df57c3b3304deadc
Nazifullah <nazif.it@gmail.com>
2023-11-03 03:54:56 +0530

    handle games with no Image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,5 +1,7 @@
+import NoImg from "../assets/logo.png";
+
 const getCroppedImageUrl = (url: string) => {
-  if (!url) return "";
+  if (!url) return NoImg;
   const target = "media/";
   const index = url.indexOf(target) + target.length;
   return url.slice(0, index) + "crop/600/400/" + url.slice(index);
