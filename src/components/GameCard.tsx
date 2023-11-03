409e5d40cf9efe869323743d636f8db398adfc46
Nazifullah <nazif.it@gmail.com>
2023-11-03 12:50:57 +0530

    cleanup the game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -13,7 +13,6 @@ const GameCard = ({ game }: Props) => {
       <Card>
         <Image src={getCroppedImageUrl(game.background_image)} />
         <CardBody>
-          <Heading fontSize="2xl">{game.name}</Heading>
           {/* {game.parent_platforms.map(({ platform }) => (
             <Text> {platform.name} </Text>
           ))} */}
@@ -21,12 +20,13 @@ const GameCard = ({ game }: Props) => {
           {game.parent_platforms.map((platform)=> platform.platform.name)} */}
           {/* now to pass it to another component "platformIconList we have to create and 
           array of ojects as: " */}
-          <HStack justifyContent={"space-between"}>
+          <HStack justifyContent={"space-between"} marginBottom={3}>
             <PlatformIconList
               platforms={game.parent_platforms.map((p) => p.platform)}
             />
             <CriticScore score={game.metacritic} />
           </HStack>
+          <Heading fontSize="2xl">{game.name}</Heading>
         </CardBody>
       </Card>
     </>
