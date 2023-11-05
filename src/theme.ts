90a1a78bb86b0c40aeded93ec47c7b4f7af0eb8b
Nazifullah <nazif.it@gmail.com>
2023-11-06 01:50:57 +0530

    customizeing the theme

--- src/theme.ts
+++ src/theme.ts
@@ -4,6 +4,22 @@ const config: ThemeConfig = {
   initialColorMode: "dark",
 };
 
-const theme = extendTheme({ config });
+const theme = extendTheme({
+  config,
+  colors: {
+    gray: {
+      50: " #ebf7eb",
+      100: " #cce6cc",
+      200: "#acd3ac",
+      300: " #8bc38b",
+      400: "#6ab26a",
+      500: "#529851",
+      600: "#3f763f",
+      700: "#1a331a",
+      800: "#111",
+      900: "#051205",
+    },
+  },
+});
 
 export default theme;
