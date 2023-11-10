5a5ca0c949bbc9a346272db4bbd57746fed24b6b
Nazifullah <nazif.it@gmail.com>
2023-11-11 03:00:43 +0530

    installed react query- tools and linting

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,11 +1,12 @@
-import genre from "../data/genre";
+// import genre from "../data/genre";
 
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
-const useGenres = () => ({ data: genre, isLaoding: false, error: null });
+// const useGenres = () => ({ data: genre, isLaoding: false, error: null });
+const useGenres = () => {};
 export default useGenres;
 
 // second part starts from here
