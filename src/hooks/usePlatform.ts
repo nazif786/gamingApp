932d7515aaa1ab297a67354990b407dce2ede25b
Nazifullah <nazif.it@gmail.com>
2023-11-11 04:37:35 +0530

    using static data for usePlatfrom hook

--- src/hooks/usePlatform.ts
+++ src/hooks/usePlatform.ts
@@ -1,11 +1,15 @@
-import useData from "./useData";
-
+// import useData from "./useData";
+import platforms from "../data/platforms";
 interface Platforms {
   id: number;
   name: string;
   slug: string;
 }
-
-const usePlatfrom = () => useData<Platforms>("/platforms/lists/parents");
+// 1. gettin data using react- useEffect and react useState
+// const usePlatfrom = () => useData<Platforms>("/platforms/lists/parents");
+// 2. fetching dta using static file ../data/platfroms
+// const usePlatfrom = () => ({ data: platforms, isLoading: false, error: null });
+// 3. fetching data using React-Query
+const usePlatfrom = () => ({ data: platforms, isLoading: false, error: null });
 
 export default usePlatfrom;
